b838a87e2a60aa5342da5d5e088ff6c35cbb3190
Aymon de Broglie <aymon.debroglie@telecom-paristech.fr>
2015-06-15 15:22:07 +0200

    La classe retourne les prénoms

--- src/JDOM2.java
+++ src/JDOM2.java
@@ -11,35 +11,18 @@ import java.util.Iterator;
 public class JDOM2
 {
    static org.jdom2.Document document;
-   static Element racine;
 
    public static void main(String[] args)
    {
-      //On crée une instance de SAXBuilder
-      SAXBuilder sxb = new SAXBuilder();
-      try
-      {
-         //On crée un nouveau document JDOM avec en argument le fichier XML
-         //Le parsing est terminé ;)
-         document = sxb.build(new File("D:\\PAF\\Git PAf\\src\\2706406.xml"));
-      }
-      catch(Exception e){}
-      
-
-      //On initialise un nouvel élément racine avec l'élément racine du document.
-     racine = document.getRootElement();
-     racine = racine.getChild("fr-bibliographic-data");
-     racine = racine.getChild("parties");
-     racine = racine.getChild("inventors");
+  
+   File repertoire =new File("C:\\Users\\Aymon\\Desktop\\Test");
+   listerRepertoire(repertoire);
      		
    
-
-      //Méthode définie dans la partie 3.2. de cet article
-      afficheALL();
    }
    
  //Ajouter cette méthodes à la classe JDOM2
-   static void afficheALL()
+   static void afficheALL(Element racine)
    {
       //On crée une List contenant tous les noeuds "etudiant" de l'Element racine
       List listEtudiants = racine.getChildren("inventor");
@@ -55,4 +38,36 @@ public class JDOM2
          System.out.println(courant.getChild("addressbook").getChild("first-name").getText());
       }
    }
+   
+   public static void listerRepertoire(File repertoire){ 
+
+	   String [] listefichiers; 
+
+	   int i; 
+	   listefichiers=repertoire.list(); 
+	   for(i=0;i<listefichiers.length;i++){ 
+	   if(listefichiers[i].endsWith(".xml")==true){ 
+       
+		   //On crée une instance de SAXBuilder
+		      SAXBuilder sxb = new SAXBuilder();
+		      try
+		      {
+		         //On crée un nouveau document JDOM avec en argument le fichier XML
+		         //Le parsing est terminé ;)
+		         document = sxb.build(new File("C:\\Users\\Aymon\\Desktop\\Test\\"+listefichiers[i]));
+		      }
+		      catch(Exception e){}
+		      Element racine;
+
+		      //On initialise un nouvel élément racine avec l'élément racine du document.
+		    try{ racine = document.getRootElement();
+		     racine = racine.getChild("fr-bibliographic-data");
+		     racine = racine.getChild("parties");
+		     racine = racine.getChild("inventors");
+		     racine.getAttribute("inventors");
+		     afficheALL(racine);}
+		     catch (Exception e){}
+	   } 
+	   } 
+	   }
 }
