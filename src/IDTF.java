53a7d55b07a3e768973d9f34f643ef37d78e0bad
mbousbaine <mehdi.bousbaine@telecom-paristech.fr>
2015-06-16 14:18:23 +0200

    Permet de stocker tous les titres dans une liste, debut implementation methode IDTF

---
+++ src/IDTF.java
@@ -0,0 +1,114 @@
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.util.ArrayList;
+
+
+
+public class IDTF {
+	
+	  public double tfCalculator(ArrayList<String> totalterms, String termToCheck) {
+	        double count = 0;  //to count the overall occurrence of the term termToCheck
+	        for (String s : totalterms) {
+	            if (s.equalsIgnoreCase(termToCheck)) {
+	                count++;
+	            }
+	        }
+	        return count / totalterms.size();
+	    }
+	  
+	  
+	    /**
+	     * Calculates idf of term termToCheck
+	     * @param allTerms : all the terms of all the documents
+	     * @param termToCheck
+	     * @return idf(inverse document frequency) score
+	     */
+	    public double idfCalculator(ArrayList<String> allTerms, String termToCheck) {
+	        double count = 0;
+	        for (String ss : allTerms) {
+	           // for (String s : ss) {
+	                if (ss.equalsIgnoreCase(termToCheck)) {
+	                    count++;
+	                    break;
+	                }
+	            }
+	      //  }
+	        return 1 + Math.log(allTerms.size() / count);
+	    }
+	
+
+	
+	
+	  
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		
+		ArrayList<String> listTitre = new ArrayList<String>();
+		
+		try {
+            
+			File repertoire= new File("C:/Users/Mehdi/Desktop/Brev");
+			 String[] listefichiers = repertoire.list();
+            
+            
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            
+            for (int k=0; k<listefichiers.length;k++)
+            {
+            	 if(listefichiers[k].endsWith(".xml")==true)
+            	 {             
+            File fXmlFile = new File("C:/Users/Mehdi/Desktop/Brev/"+listefichiers[k]);
+            Document doc = dBuilder.parse(fXmlFile);
+            
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("fr-bibliographic-data");
+            
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                     
+                    Node nNode = nList.item(temp);
+     
+                 //   System.out.println("\nCurrent Element :" + nNode.getNodeName());
+     
+                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                            
+                            Element eElement = (Element) nNode;
+                           
+                                    
+                            
+                           // System.out.println("Titre : " + eElement.getElementsByTagName("invention-title").item(0).getTextContent());
+                            listTitre.add(eElement.getElementsByTagName("invention-title").item(0).getTextContent().toLowerCase());
+                        
+                            
+                    }
+                    
+                    
+
+            }
+            }
+            	 }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      }
+     
+    
+    
+
+
+}
