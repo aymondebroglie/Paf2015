cfdc093f1826342f52ddb883c8fe5d4f38881f59
mbousbaine <mehdi.bousbaine@telecom-paristech.fr>
2015-06-17 14:30:07 +0200

    Modif

--- src/IDTF.java
+++ src/IDTF.java
@@ -10,10 +10,11 @@ import org.w3c.dom.Element;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 
 
-public class IDTF   {
+public class IDTF    {
 	
 
 	
@@ -89,7 +90,7 @@ public class IDTF   {
 		
 		try {
             
-			File repertoire= new File("C:/Users/Mehdi/Desktop/Brev");
+			File repertoire= new File("C:/Users/Mehdi/Desktop/Bb");
 			 String[] listefichiers = repertoire.list();
             
             
@@ -100,11 +101,11 @@ public class IDTF   {
             {
             	 if(listefichiers[k].endsWith(".xml")==true)
             	 {             
-            File fXmlFile = new File("C:/Users/Mehdi/Desktop/Brev/"+listefichiers[k]);
+            File fXmlFile = new File("C:/Users/Mehdi/Desktop/Bb/"+listefichiers[k]);
             Document doc = dBuilder.parse(fXmlFile);
             
             doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("fr-bibliographic-data");
+            NodeList nList = doc.getElementsByTagName("abstract");
             
             for (int temp = 0; temp < nList.getLength(); temp++) {
                      
@@ -118,7 +119,7 @@ public class IDTF   {
                            
                                     
                             
-                           String s= eElement.getElementsByTagName("invention-title").item(0).getTextContent();
+                           String s= eElement.getElementsByTagName("p").item(0).getTextContent();
                            String[] parts= s.split(" ");
                            listTitre.add(parts);
                            for (int k1=0; k1<parts.length; k1++) {
@@ -144,6 +145,7 @@ public class IDTF   {
 		
 		String c=tout.get(0);
 		tout.removeAll(Collections.singleton(c));
+		
 		ArrayList<IDTF> repFre= new ArrayList<IDTF>();
 		int p=0;
         for (String s : tout) 
@@ -157,17 +159,16 @@ public class IDTF   {
     		p++;
 
         }
-
        
         
         
 		
 		
       }
-	
-	
+
+
      
-    
+
     
 
 
