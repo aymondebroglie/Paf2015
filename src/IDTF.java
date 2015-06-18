2156de30dc7293e149f691f8af9c41d1710ee7ad
mbousbaine <mehdi.bousbaine@telecom-paristech.fr>
2015-06-18 10:53:39 +0200

    Correction calcul de tf, reste a mettre les documents en forme avec leurs valeurs correspondantes de IDTF

--- src/IDTF.java
+++ src/IDTF.java
@@ -7,10 +7,21 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
+
+
+
+
+
+
+
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 
 
 
@@ -18,9 +29,28 @@ public class IDTF    {
 	
 
 	
-	
+	private double idf;
+	private double tf;
 	private double idtf;
 	private String mot;
+	
+	
+	public double getIdf() {
+		return idf;
+	}
+
+	public void setIdf(double idf) {
+		this.idf = idf;
+	}
+
+	public double getTf() {
+		return tf;
+	}
+
+	public void setTf(double tf) {
+		this.tf = tf;
+	}
+	
 	
 	public IDTF(String mot, double idtf)
 	{
@@ -29,6 +59,12 @@ public class IDTF    {
 		
 	}
 	
+	
+	
+	public IDTF() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public void setIdtf(double idtf){
 		this.idtf=idtf;
 		
@@ -43,22 +79,22 @@ public class IDTF    {
 	}
 	
 	
-	  public static double tfCalculator(ArrayList<String> totalterms, String termToCheck) {
-	        double count = 0;  //to count the overall occurrence of the term termToCheck
+	  public static double tfCalculator(String[] totalterms, String termToCheck) {
+	        double count = 0;  //pour compter le nombre d'occurence du terme recherché
 	        for (String s : totalterms) {
 	            if (s.equalsIgnoreCase(termToCheck)) {
 	                count++;
 	            }
 	        }
-	        return count / totalterms.size();
+	        return count / totalterms.length;
 	    }
 	  
 	  
 	    /**
-	     * Calculates idf of term termToCheck
-	     * @param allTerms : all the terms of all the documents
+	     * Calcule idf du term termToCheck
+	     * @param allTerms : tous les termes de tous les documents
 	     * @param termToCheck
-	     * @return idf(inverse document frequency) score
+	     * @return idf(inverse document frequency) resultat
 	     */
 	    public static double idfCalculator(ArrayList<String[]> allTerms, String termToCheck) {
 	        double count = 0;
@@ -70,23 +106,73 @@ public class IDTF    {
 	                }
 	            }
 	        }
-	        return Math.log(allTerms.size() / count);
+	        return Math.log((allTerms.size()+1) /1+ count);
 	    }
 	    
-	    
-	
 	  
-
+	    
+	    
+	    //tri**********************************************************************	  
+	    public static ArrayList<IDTF> fusion(ArrayList<IDTF> tab1, ArrayList<IDTF> tab2)
+	    {
+	        int taille_g=tab1.size();
+	        int taille_d=tab2.size();
+	        ArrayList<IDTF> res= new ArrayList<IDTF>();
+	        int i_g=0;
+	        int i_d=0;
+	        int i;
+	        for( i=0;i_g<taille_g && i_d<taille_d;i++)
+	            if(tab1.get(i_g).getIdtf() <= tab2.get(i_d).getIdtf())
+	                res.add(i, tab1.get(i_g++));
+	            else
+	            	res.add(i, tab2.get(i_d++));
+	            /* on copie le reste du tableau de gauche (s'il reste quelque chose) */
+	            while(i_g<taille_g)
+	            	res.add(i++, tab1.get(i_g++));
+	            while(i_d<taille_d)
+	            	res.add(i++, tab2.get(i_d++));		          
+	        return res;
+	    }
+	     
+	    public static ArrayList<IDTF> copie(ArrayList<IDTF> tab, int debut, int fin)
+	    {
+	    	ArrayList<IDTF> res= new ArrayList<IDTF>(); 
+	        for(int i=debut;i<=fin;i++)
+	        {
+	        	res.add(i-debut, tab.get(i));
+	        }
+	        return res;
+	    }
+	 
+	    public static ArrayList<IDTF> tri_fusion(ArrayList<IDTF> tab)
+	    {
+	        int taille = tab.size();
+	        if(taille<=1)
+	            return tab;
+	        else
+	        {
+	            int mileu = taille/2;
+	            ArrayList<IDTF> gauche = copie(tab,0,mileu-1);
+	            ArrayList<IDTF> droite = copie(tab,mileu,taille-1);
+	            return fusion(tri_fusion(gauche),tri_fusion(droite));
+	        }
+	    }
+     //fin tri**********************************************************************************
+	    
+	    
+	    
+	    
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+	
+		ArrayList<String[]> listTitre = new ArrayList<String[]>();   // liste qui contient la description de chaque brevet: 1 élement = 1 brevet
+		ArrayList<String> tout= new ArrayList<String>();             // liste qui contient tous les mots de chaque brevet
+		int tab[] = null;
 		
 		
-
 		
-		ArrayList<String[]> listTitre = new ArrayList<String[]>();
-		ArrayList<String> tout= new ArrayList<String>();
 		
 		try {
             
@@ -97,7 +183,7 @@ public class IDTF    {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             
-            for (int k=0; k<listefichiers.length/100;k++)
+            for (int k=0; k<listefichiers.length;k++)
             {
             	 if(listefichiers[k].endsWith(".xml")==true)
             	 {             
@@ -119,11 +205,13 @@ public class IDTF    {
                            
                                     
                             
-                           String s= eElement.getElementsByTagName("p").item(0).getTextContent();
+                           String s= eElement.getElementsByTagName("p").item(0).getTextContent().toLowerCase();
                            String[] parts= s.split(" ");
                            listTitre.add(parts);
-                           for (int k1=0; k1<parts.length; k1++) {
+                           for (int k1=0; k1<parts.length; k1++) 
+                           {
                         	   tout.add(parts[k1]);
+                        	   
                            }
                            
                         
@@ -143,34 +231,89 @@ public class IDTF    {
             e.printStackTrace();
         }
 		
-		String c=tout.get(0);
-		tout.removeAll(Collections.singleton(c));
-		
+		//String c=tout.get(0);
+		//tout.removeAll(Collections.singleton(c));
 		ArrayList<IDTF> repFre= new ArrayList<IDTF>();
-		int p=0;
+		ArrayList<IDTF> repFreq= new ArrayList<IDTF>();
+	//	int p=0;
         for (String s : tout) 
         {
+
     		double idfCalculator = idfCalculator(listTitre, s);
-    		double tfCalculator= tfCalculator(tout, s);
+    		IDTF idtf= new IDTF(s, idfCalculator);
+    		repFre.add(idtf);
+
+    		
+    		/*double tfCalculator= tfCalculator(listTitre.get(p), s);
     		double f= idfCalculator*tfCalculator;
     		IDTF idtf= new IDTF(s, f);
     		repFre.add(idtf);
-    		System.out.println(repFre.get(p).getMot() +"  "+ repFre.get(p).getIdtf());
-    		p++;
+    		//System.out.println(repFre.get(p).getMot() +"   "+ repFre.get(p).getIdtf());
+    		p++;*/
 
         }
        
         
+        for (int p=0; p< listTitre.size();p++) 
         
+        {
+        	
+        	for (int t=0; t< listTitre.get(p).length; t++)
+        	{  	
+        	double tf_p= tfCalculator(listTitre.get(p), listTitre.get(p)[t] );
+        	IDTF idtf= new IDTF(listTitre.get(p)[t],tf_p );
+        	repFreq.add(idtf);  	
+        	}
+        	
+        }
+        
+        
+       
+        /*
+        Set<String> set = new HashSet<String>() ;
+        set.addAll(tout) ;
+        ArrayList<String> repFre1 = new ArrayList<String>(set) ;
+
+        */
+        
+        
+        
+    //   repFreq= tri_fusion(repFre);
+        
+
+
 		
+        
+     
+      
+        
+        
+        
+        
+        
+        for(int u=0;u<repFre.size();u++){
+        	System.out.println(repFre.get(u).getIdtf()+ "    "+ repFre.get(u).getMot());
+        	System.out.println(repFreq.get(u).getIdtf()+ "    "+ repFreq.get(u).getMot());
+
+
+        }
+        
+        
+        for (int y=0; y<repFreq.size(); y++){
+         	System.out.println(repFreq.get(y).getIdtf()+ "    "+ repFreq.get(y).getMot());
+        	
+        }
+        
 		
       }
 
+	
+
 
      
-
+}
     
 
 
-}
+
 
